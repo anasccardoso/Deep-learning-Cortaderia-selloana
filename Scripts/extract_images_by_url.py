c151598cb0fe8ddbed6f7760951697694064d72b
Ana Sofia Cardoso <75045783+anasccardoso@users.noreply.github.com>
2023-09-15 15:36:45 +0100

    Add files via upload

---
+++ Scripts/extract_images_by_url.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import requests
+
+df = pd.read_excel("~/iNaturalist_dataset.xlsx")
+print(df)
+
+i = 0
+for url in df["image_url"]:
+    img_data = requests.get(url).content
+    with open('image' + str(i) + '.jpg', 'wb') as handler:
+        handler.write(img_data)
+        i += 1
